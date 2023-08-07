cd466e189a7b5101bf101106eeef0965a920b0a2
Josseph Rojas <josseph_14@hotmail.com>
2023-08-06 22:18:01 -0500

    1

---
+++ main.go
@@ -0,0 +1,359 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"text/template"
+)
+
+var (
+	host          = "127.0.0.1"
+	port          = "3306"
+	user          = "dbadmin"
+	passroot      = "Holacomoestan30*"
+	dbname        = "challenge"
+	client_secret = "C:/client_secret.json"
+	/*host      = os.Getenv("host")
+	port      = os.Getenv("port")
+	user      = os.Getenv("user")
+	passroot  = os.Getenv("passroot")
+	dbname    = os.Getenv("dbname")
+	client_secret = os.Getenv("client_secret")*/
+	mysqlInfo      = fmt.Sprintf(user + ":" + passroot + "@tcp(" + host + ":" + port + ")/" + dbname)
+	filePathKEY    string
+	filePathCERT   string
+	tmpl           = template.Must(template.ParseGlob("template/*"))
+	authURL        string
+	idDatoToken    string
+	table          = Table{}
+	arraytable     []Table
+	arraytable2    []Table
+	data           Data
+	ID             string
+	FileID         string
+	FileName       string
+	Extension      string
+	FileOwner      string
+	Visibility     int
+	ViewFile       string
+	Classification sql.NullString
+	Version        string
+	CreatedTime    string
+	ModifiedTime   string
+	QuotaBytesUsed string
+	srv            *drive.Service
+)
+
+type Data struct {
+	Suma    int    `json:"suma"`
+	File_ID string `json:"otroValor"`
+}
+type Table struct {
+	ID             string
+	FileID         string
+	FileName       string
+	Extension      string
+	FileOwner      string
+	Visibility     string
+	ViewFile       string
+	Classification string
+	Version        string
+	CreatedTime    string
+	ModifiedTime   string
+	QuotaBytesUsed string
+}
+
+func Err(err2 error) {
+	if err2 != nil {
+		log.Println(err2)
+	}
+}
+func Certificados() {
+	currentDir, err := os.Getwd()
+	Err(err)
+	filePathKEY = filepath.Join(currentDir, "/certificados/server.key")
+	filePathCERT = filepath.Join(currentDir, "/certificados/certbundle.pem")
+
+}
+func ConexionDB() (db *sql.DB) {
+	db, err := sql.Open("mysql", mysqlInfo)
+	log.Println("Conectando con Base de Datos")
+	Err(err)
+	return db
+}
+func URLGDrive() {
+	credentialsJSON, err := ioutil.ReadFile(client_secret)
+	credentials, err := google.ConfigFromJSON(credentialsJSON, drive.DriveScope)
+	Err(err)
+	authURL = credentials.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+
+}
+func redirectToHttps(writer http.ResponseWriter, request *http.Request) {
+	http.Redirect(writer, request, "https://localhost:443"+request.RequestURI, http.StatusMovedPermanently)
+	log.Println("Redireccion a HTTPS")
+}
+
+func main() {
+
+	Certificados()
+	CrearTablaInventario()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", Inicio)
+	r.HandleFunc("/clasificacion", Clasificacion)
+	r.HandleFunc("/listado", Listado)
+
+	go func() {
+		_ = http.ListenAndServeTLS(":443", filePathCERT, filePathKEY, r)
+
+	}()
+	_ = http.ListenAndServe(":80", http.HandlerFunc(redirectToHttps))
+
+}
+
+func CrearTablaInventario() {
+	db := ConexionDB()
+	defer func(db *sql.DB) {
+		_ = db.Close()
+	}(db)
+
+	log.Println("Creando (si no existe) la tabla inventario")
+	createTableQuery := `CREATE TABLE IF NOT EXISTS inventario (
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            FileID TEXT,
+                            FileName TEXT,    
+							Extension TEXT,   
+							FileOwner TEXT,
+							Visibility INT,
+							Classification TEXT,
+							ViewFile TEXT,
+                            Version TEXT,
+                            CreatedTime TEXT,
+                            ModifiedTime TEXT,
+                            QuotaBytesUsed TEXT
+                         );`
+	_, err := db.Exec(createTableQuery)
+	Err(err)
+}
+func ConexionGDrive() {
+	ctx := context.Background()
+	//timeoutCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	//defer cancel()
+	credentialsJSON, err := ioutil.ReadFile(client_secret)
+	credentials, err := google.ConfigFromJSON(credentialsJSON, drive.DriveScope)
+	Err(err)
+
+	if idDatoToken != "" {
+		token, err := credentials.Exchange(context.Background(), idDatoToken)
+		Err(err)
+
+		client := credentials.Client(ctx, token)
+
+		srv, err = drive.New(client)
+		Err(err)
+
+		fileList, err := srv.Files.List().Fields("files(id, name, fileExtension, properties)").Do()
+		Err(err)
+		for _, file := range fileList.Files {
+			if file.FileExtension != "" {
+				if len(file.Properties) == 0 {
+					properties := make(map[string]string)
+					properties["clasificacion"] = "sin clasificar"
+					updateFile := &drive.File{
+						Properties: properties,
+					}
+					_, err := srv.Files.Update(file.Id, updateFile).Fields("properties").Do()
+					Err(err)
+				}
+			}
+		}
+
+		db := ConexionDB()
+		defer func(db *sql.DB) {
+			_ = db.Close()
+		}(db)
+
+		_, err = db.Exec("DELETE FROM inventario")
+		Err(err)
+		fileList2, err := srv.Files.List().Fields("files(id, name, fileExtension, webViewLink , owners, shared, properties,version,createdTime,modifiedTime,quotaBytesUsed)").Do()
+		Err(err)
+
+		for _, file := range fileList2.Files {
+			if file.FileExtension != "" {
+				for _, owner := range file.Owners {
+					insertQuery := "INSERT INTO inventario (FileID,FileName,Extension,FileOwner,Visibility,ViewFile,Version,CreatedTime,ModifiedTime,QuotaBytesUsed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+					_, err := db.Exec(insertQuery, file.Id, file.Name, file.FileExtension, owner.EmailAddress, file.Shared, file.WebViewLink, file.Version, file.CreatedTime, file.ModifiedTime, file.QuotaBytesUsed)
+					Err(err)
+
+				}
+			}
+		}
+
+		for _, file := range fileList2.Files {
+			if file.FileExtension != "" {
+				for _, properties := range file.Properties {
+					updateQuery := "UPDATE inventario SET Classification = ? WHERE FileID = ?"
+					_, err := db.Exec(updateQuery, properties, file.Id)
+					Err(err)
+				}
+			}
+		}
+
+	}
+}
+func Inicio(w http.ResponseWriter, r *http.Request) {
+
+	URLGDrive()
+	_ = tmpl.ExecuteTemplate(w, "inicio", authURL)
+	idDatoToken = r.URL.Query().Get("code")
+	ConexionGDrive()
+}
+func Listado(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodPost {
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&data)
+		if err != nil {
+			http.Error(w, "Error al decodificar los datos", http.StatusBadRequest)
+			return
+		}
+		//fmt.Print(strconv.Itoa(data.Suma) + "\n")
+	}
+
+	Modificacion(strconv.Itoa(data.Suma), data.File_ID)
+
+	db := ConexionDB()
+	defer func(db *sql.DB) {
+		_ = db.Close()
+	}(db)
+	arraytable = nil
+	registros, err := db.Query("Select ID,FileID,FileName,Extension,FileOwner,Visibility,Classification,ViewFile From inventario")
+	Err(err)
+	log.Println("Pagina de listado")
+	for registros.Next() {
+		err = registros.Scan(&ID, &FileID, &FileName, &Extension, &FileOwner, &Visibility, &Classification, &ViewFile)
+		Err(err)
+
+		table.ID = ID
+		table.FileID = FileID
+		table.FileName = FileName
+		table.Extension = Extension
+		table.FileOwner = FileOwner
+		if Visibility == 1 {
+			table.Visibility = "Público"
+		} else {
+			table.Visibility = "Privado"
+		}
+		table.ViewFile = ViewFile
+		if Classification.Valid {
+			table.Classification = Classification.String
+		} else {
+			table.Classification = "Permisos Insuficientes"
+		}
+		arraytable = append(arraytable, table)
+	}
+	_ = tmpl.ExecuteTemplate(w, "listado", arraytable)
+}
+func Clasificacion(w http.ResponseWriter, r *http.Request) {
+	idDato := r.URL.Query().Get("id")
+
+	db := ConexionDB()
+	defer func(db *sql.DB) {
+		_ = db.Close()
+	}(db)
+	query := "Select FileID,FileName,FileOwner,Visibility,Classification,ViewFile,Version,CreatedTime,ModifiedTime,QuotaBytesUsed From inventario where id = ?"
+	registros, err := db.Query(query, idDato)
+	Err(err)
+	arraytable2 = nil
+	log.Println("Pagina de Informacion")
+	for registros.Next() {
+		err = registros.Scan(&FileID, &FileName, &FileOwner, &Visibility, &Classification, &ViewFile, &Version, &CreatedTime, &ModifiedTime, &QuotaBytesUsed)
+		Err(err)
+
+		table.FileID = FileID
+		table.FileName = FileName
+		table.FileOwner = FileOwner
+		if Visibility == 1 {
+			table.Visibility = "Público"
+		} else {
+			table.Visibility = "Privado"
+		}
+		table.ViewFile = ViewFile
+		if Classification.Valid {
+			table.Classification = Classification.String
+		} else {
+			table.Classification = "Permisos Insuficientes"
+		}
+		table.Version = Version
+		table.CreatedTime = CreatedTime
+		table.ModifiedTime = ModifiedTime
+		table.QuotaBytesUsed = QuotaBytesUsed
+
+		arraytable2 = append(arraytable2, table)
+	}
+
+	_ = tmpl.ExecuteTemplate(w, "clasificacion", arraytable2)
+
+}
+func Modificacion(data string, fileid string) {
+	switch data {
+	case "1":
+		InsertarCriticidad("Bajo", fileid)
+	case "2":
+		InsertarCriticidad("Medio", fileid)
+	case "3":
+		InsertarCriticidad("Alto", fileid)
+	case "4":
+		InsertarCriticidad("Critico", fileid)
+	default:
+		log.Println("No se clasificó")
+	}
+}
+func InsertarCriticidad(criticidad string, idArchivo string) {
+
+	db := ConexionDB()
+	defer func(db *sql.DB) {
+		_ = db.Close()
+	}(db)
+	updateQuery := "UPDATE inventario SET Classification = ? WHERE FileID = ?"
+	_, err := db.Exec(updateQuery, criticidad, idArchivo)
+	Err(err)
+
+	if idArchivo != "" {
+		properties := make(map[string]string)
+		properties["clasificacion"] = criticidad
+		updateFile := &drive.File{
+			Properties: properties,
+		}
+		_, err = srv.Files.Update(idArchivo, updateFile).Fields("properties").Do()
+		Err(err)
+		log.Println("Archivo clasificado")
+		if criticidad == "Critico" {
+			privado := 0
+			updateQuery := "UPDATE inventario SET Visibility = ? WHERE FileID = ?"
+			_, err := db.Exec(updateQuery, privado, idArchivo)
+			Err(err)
+
+			nuevoShared := false
+			updateFile := &drive.File{
+				Shared: nuevoShared,
+			}
+			_, err = srv.Files.Update(idArchivo, updateFile).Fields("shared").Do()
+			Err(err)
+		}
+	}
+
+}
